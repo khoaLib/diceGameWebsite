c084f04470d507f177728651465ee5b1d7908cf0
DANG KHOA <dangkhoa@DANGs-MacBook-Pro.local>
2020-08-16 04:32:57 -0700

    better version of the game

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ var changeImage = "images/dice" + randomNumber1 + ".png";
 document.querySelector(".img1").src = changeImage;
 
 changeImage = "images/dice" + randomNumber2 + ".png";
-
 document.querySelector(".img2").src = changeImage;
+
+if(randomNumber1<randomNumber2){
+  document.querySelector("h1").textContent = "Player 2 Win 🍾";
+}else if(randomNumber1>randomNumber2){
+  document.querySelector("h1").textContent = "🍾 Player 1 Win";
+}else{
+  document.querySelector("h1").textContent = "🍾 Draw match!! 🍾";
+}
